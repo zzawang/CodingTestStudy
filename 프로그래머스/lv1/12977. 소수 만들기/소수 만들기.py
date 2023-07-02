1099f3f80bc527deac34df28a81eb9487df6623e
최호빈 <103445254+zzawang@users.noreply.github.com>
2023-07-02 18:00:11 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -1,12 +1,8 @@
+from itertools import combinations
+
 def solution(nums):
-    num = []
-    
-    for i1 in range(len(nums) - 2):
-        for i2 in range(i1 + 1, len(nums) - 1):
-            for i3 in range(i2 + 1, len(nums)):
-                num.append(nums[i1] + nums[i2] + nums[i3])
-    
-    return sum(yaksu(v) for v in num)
+    num = list(combinations(nums, 3))
+    return sum(yaksu(sum(v)) for v in num)
 
 def yaksu(n):
     boolean = False
