c6f3fe62eb41ea8fbebbca92b9550bdd60747e68
최호빈 <hobinchoi0116@gmail.com>
2023-07-23 22:24:58 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,27 +1,29 @@
 def solution(id_list, report, k):
+    answer = {}
+    # 본인이 신고한 사람들
+    To = {}
+    # 본인이 신고받은 횟수
+    From = {}
     
-    id_dict = {}
-    report_dict = {}
-    
-    for id in id_list:
-        id_dict[id] = 0
-        report_dict[id] = list()
-        # {"muzi" : ["xx", "dd"], "frodo" : ["dd"]} .. 이런식
-    
-    for content in report:
-        a, b = content.split(" ")
-        # 자기 자신을 신고한 사람을 추가
-        if a not in report_dict[b]:
-            report_dict[b].append(a)
-            
-    for i in report_dict:
-        if len(report_dict[i]) >= k:
-            for x in report_dict[i]:
-                id_dict[x] += 1
-            
-    answer = []
-    
-    for p in id_dict:
-        answer.append(id_dict[p])
+    for i in id_list:
+        answer[i] = 0
+        To[i] = []
+        From[i] = 0
         
-    return answer
+    for r in report:
+        t, f = r.split(' ')
+        if f not in To[t]:
+            To[t].append(f)
+            From[f] += 1
+        
+    find = []
+    for f in From:
+        if From[f] >= k:
+            find.append(f)
+            
+    for t1 in To:
+        for t2 in To[t1]:
+            if t2 in find:
+                answer[t1] += 1
+    
+    return [x for x in answer.values()]
