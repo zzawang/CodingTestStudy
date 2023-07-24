a35d61f687c1952b4477ed8a2a43db45ed184448
최호빈 <hobinchoi0116@gmail.com>
2023-07-25 04:37:45 +0900

    [level 2] Title: 숫자의 표현, Time: 1.53 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,20 @@
+def solution(n):
+    answer = 1  # 자기 자신 추가
+    
+    left = 1
+    right = 1
+    sum = 1
+    
+    while left != n:
+        if sum < n:
+            right += 1
+            sum += right
+        elif sum > n:
+            sum -= left
+            left += 1
+        else:
+            answer += 1
+            sum -= left
+            left += 1
+    
+    return answer
