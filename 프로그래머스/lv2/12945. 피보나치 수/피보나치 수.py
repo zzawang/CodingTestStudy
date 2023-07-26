78373afa3f24dd0cffcc716cb097e1bb8755f895
최호빈 <hobinchoi0116@gmail.com>
2023-07-26 17:54:28 +0900

    [level 2] Title: 피보나치 수, Time: 7.14 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,6 +1,5 @@
-def solution(n):
-    fibo = {}
-    fibo[0], fibo[1] = 0, 1
-    for i in range(2, n + 1):
-        fibo[i] = fibo[i-1] + fibo[i-2]
-    return fibo[n]%1234567
+def solution(num):
+    a, b = 0, 1
+    for i in range(num):
+        a, b = b, (a+b)%1234567
+    return a
