0dfa6be2730179eef222c0bebe8d93565e78b08b
최호빈 <hobinchoi0116@gmail.com>
2023-07-27 23:41:43 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 138 ms, Memory: 53,096 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -1,12 +1,12 @@
 T = int(input())
 for t in range(1, T + 1):
-    find = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    find = set()
     n = int(input())
     index = 0
-    while find != []:
+    while len(find) != 10:
         index += 1
-        k = list(map(int, str((n * index))))
+        k = set(str(n * index))
         for k1 in k:
-            if k1 in find:
-                find.remove(k1)
+            if k1 not in find:
+                find.add(k1)
     print(f"#{t} {n * index}")
