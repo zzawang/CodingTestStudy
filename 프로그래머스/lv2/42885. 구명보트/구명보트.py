1e9353057d4a7a3c808f580bab2a870cbdbf3175
최호빈 <hobinchoi0116@gmail.com>
2023-07-27 21:15:10 +0900

    [level 2] Title: 구명보트, Time: 8.09 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,14 @@
+def solution(people, limit):
+    people.sort()
+    
+    answer = 0
+    left = 0
+    right = len(people) - 1
+    
+    while left < right:
+        if people[left] + people[right] <= limit:
+            left += 1
+        right -= 1
+        answer += 1
+            
+    return answer if left != right else answer + 1
