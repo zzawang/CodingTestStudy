b7fd26bf701fbb7aefdbe2e0a3df02dd64205fdc
최호빈 <hobinchoi0116@gmail.com>
2023-07-30 19:16:25 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -3,5 +3,5 @@ def solution(n,a,b):
         if min(a,b)%2 == 1 and abs(a-b) == 1:
             return i
         else:
-            a = a//2 if a%2 == 0 else (a+1)//2
-            b = b//2 if b%2 == 0 else (b+1)//2
+            a = (a+1)//2
+            b = (b+1)//2
