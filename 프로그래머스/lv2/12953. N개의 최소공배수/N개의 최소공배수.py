ef2ede624c14f6805a4994a8bcc0b782ebd41cc4
최호빈 <hobinchoi0116@gmail.com>
2023-08-01 15:39:22 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,7 +1,7 @@
 import math
 def solution(arr):
-    answer = arr[0] if len(arr) == 1 else (arr[0]*arr[1]) // math.gcd(arr[0], arr[1])
-    for i in range(2, len(arr)):
+    answer = arr[0]
+    for i in range(1, len(arr)):
         answer = (answer*arr[i]) // math.gcd(answer, arr[i])
         
     return answer
