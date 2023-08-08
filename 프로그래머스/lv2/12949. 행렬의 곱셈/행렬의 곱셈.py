f4682e7644a2192e916edd9f55a52b28e3d96c79
최호빈 <hobinchoi0116@gmail.com>
2023-08-08 21:04:02 +0900

    [level 2] Title: 행렬의 곱셈, Time: 9.51 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,3 +1,16 @@
-import numpy as np
 def solution(arr1, arr2):
-    return (np.array(arr1) @ np.array(arr2)).tolist()
+    arr = []
+    for a in arr1:
+        t1 = 0
+        t2 = 0
+        li = []
+        for _ in range(len(arr2[0])):
+            sum = 0
+            for a1 in a:
+                sum += (a1 * arr2[t1][t2])
+                t1 += 1
+            li.append(sum)
+            t2 += 1
+            t1 = 0
+        arr.append(li)
+    return arr
