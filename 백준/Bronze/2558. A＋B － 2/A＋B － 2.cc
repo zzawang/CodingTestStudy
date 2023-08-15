74a00c3f0b0ad019953e036e14db337da87c7833
최호빈 <hobinchoi0116@gmail.com>
2023-08-16 00:07:20 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,result;
+    cin>>a;
+    cin>>b;
+    if(a<=0 || b>=10)
+        return 0;
+    result=a+b;
+    cout<<result<<endl;
+    return 0;
+}
