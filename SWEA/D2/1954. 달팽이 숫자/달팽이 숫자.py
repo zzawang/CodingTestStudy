eeb7992a50a2760dc9932e54fa6c75669e5b54f8
최호빈 <hobinchoi0116@gmail.com>
2023-08-16 01:18:05 +0900

    [D2] Title: 달팽이 숫자, Time: 98 ms, Memory: 43,768 KB -BaekjoonHub

---
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -0,0 +1,28 @@
+for tc in range(1, int(input()) + 1):
+    n = int(input())
+    print(f"#{tc}")
+    if n == 1:
+        print('1')
+        continue
+    arr = [[0] * n for _ in range(n)]
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+
+    x, y = 0, -1
+    num = 0
+    index = -1
+
+    while True:
+        index = (index + 1)%4
+        if arr[x + dx[index]][y + dy[index]] != 0:
+            break
+        while 0 <= x + dx[index] < n and 0 <= y + dy[index] < n and arr[x + dx[index]][y + dy[index]] == 0:
+            num += 1
+            x += dx[index]
+            y += dy[index]
+            arr[x][y] = num
+
+    for a1 in arr:
+        for a2 in a1:
+            print(a2, end=' ')
+        print()
