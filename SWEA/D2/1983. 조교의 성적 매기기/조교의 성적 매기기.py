0b633e4e20d4e288643fbdc12f230493407828fa
최호빈 <hobinchoi0116@gmail.com>
2023-10-09 01:23:26 +0900

    [D2] Title: 조교의 성적 매기기, Time: 113 ms, Memory: 49,264 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
@@ -1,22 +1,18 @@
 for test_case in range(1, int(input()) + 1):
-    answer = {}
     score = []
-    arr = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
+    grade = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
     n, student = map(int, input().split())
 
     for i in range(n):
         mid, fin, report = map(int, input().split())
         total = (mid*0.35 + fin*0.45 + report*0.2)
-        answer[total] = i
         score.append(total)
 
-    index = 0
-    flag = 0
-    for s in sorted(score, reverse=True):
-        answer[s] = arr[index]
-        flag += 1
-        if flag == n//10:
-            index += 1
-            flag = 0
+    # 구하려는 학생의 총 점수
+    find_score = score[student-1]
 
-    print(f"#{test_case} {answer[score[student-1]]}")
+    score.sort(reverse=True)
+    idx = score.index(find_score)
+    answer = grade[idx//(n//10)]
+
+    print(f"#{test_case} {answer}")
