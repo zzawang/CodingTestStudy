dfc46afa2b68219bce707818845f54e16ce99477
최호빈 <hobinchoi0116@gmail.com>
2023-11-15 03:18:57 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -0,0 +1,13 @@
+def solution(X, Y):
+    answer = ''
+    for i in range(9, -1, -1):
+        i = str(i)
+        for _ in range(min(X.count(i), Y.count(i))):
+            answer += i
+        
+    if answer == '':
+        return "-1"
+    elif len(answer) == answer.count('0'):
+        return "0"
+    else:
+        return answer
