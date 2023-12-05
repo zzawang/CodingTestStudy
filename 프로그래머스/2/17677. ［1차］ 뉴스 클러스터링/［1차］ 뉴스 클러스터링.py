6685f924df131bb703e76d822af042bb963be976
최호빈 <hobinchoi0116@gmail.com>
2023-12-06 00:19:02 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,39 +1,22 @@
 def solution(str1, str2):
-    str1 = str1.lower();
-    str2 = str2.lower();
     A_list = []
     B_list = []
-    intersection = []
-    union = []
     
     for i in range(len(str1) - 1):
         if str1[i:i+2].isalpha():
-            A_list.append(str1[i:i+2])
+            A_list.append(str1[i:i+2].lower())
         
     for i in range(len(str2) - 1):
         if str2[i:i+2].isalpha():
-            B_list.append(str2[i:i+2])
-            
-    for s1 in A_list:
-        if s1 not in intersection:
-            for _ in range(min(A_list.count(s1), B_list.count(s1))):
-                intersection.append(s1)
-                
-        if s1 not in union:
-            for _ in range(max(A_list.count(s1), B_list.count(s1))):
-                union.append(s1)
-                
-    for s1 in B_list:
-        if s1 not in intersection:
-            for _ in range(min(A_list.count(s1), B_list.count(s1))):
-                intersection.append(s1)
-                
-        if s1 not in union:
-            for _ in range(max(A_list.count(s1), B_list.count(s1))):
-                union.append(s1)
+            B_list.append(str2[i:i+2].lower())
     
-    if not union:
-        answer = 1
+    intersection = set(A_list) & set(B_list)
+    union = set(A_list) | set(B_list)
+    
+    intersection_sum = sum([min(A_list.count(u), (B_list.count(u))) for u in intersection])
+    union_sum = sum([max(A_list.count(i), (B_list.count(i))) for i in union])
+                
+    if union_sum == 0:
+        return 65536
     else:
-        answer = len(intersection) / len(union)
-    return int(answer * 65536)
+        return int((intersection_sum / union_sum) * 65536)
