910dcdc38b39dceb6f3b3903826d309048f34959
최호빈 <hobinchoi0116@gmail.com>
2023-12-16 21:51:53 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -2,16 +2,13 @@ def solution(skill, skill_trees):
     answer = 0
     
     for sts in skill_trees:
-        stack = [s for s in skill][::-1]
-        flag = True
+        stack = [s for s in skill]
         for st in sts:
-            if stack and st in stack and st != stack[-1]:
-                flag = False
+            if stack and st in stack and st != stack[0]:
                 break
-            elif stack and st in stack and st == stack[-1]:
-                stack.pop()
-        
-        if flag:
+            elif stack and st in stack and st == stack[0]:
+                stack.pop(0)
+        else:
             answer += 1
     
     return answer
