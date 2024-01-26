1dac84e8e19f10f74766ac47a2c01defad30208c
최호빈 <hobinchoi0116@gmail.com>
2024-01-26 17:47:31 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 68.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                if (i == Math.sqrt(n)) {
+                    answer += i;
+                } else {
+                    answer += i;
+                    answer += (n / i);
+                }
+            }
+        }
+        return answer;
+    }
+}
