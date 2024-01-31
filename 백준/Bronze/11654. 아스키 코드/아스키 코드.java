b76074609c7e57fde46848ae7f783e9366e0ffe8
최호빈 <hobinchoi0116@gmail.com>
2024-01-31 10:54:41 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char ascii = scanner.next().charAt(0);
+        System.out.print((int) ascii);
+    }
+}
