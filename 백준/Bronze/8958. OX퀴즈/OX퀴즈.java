1fe4e93f784ac93a1c9fb626bad9b905872bb6ed
최호빈 <hobinchoi0116@gmail.com>
2024-01-31 10:23:33 +0900

    [Bronze II] Title: OX퀴즈, Time: 256 ms, Memory: 18368 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = Integer.parseInt(scanner.nextLine());
+
+        while (count-- > 0) {
+            String[] testcase = scanner.nextLine().split("");
+            int answer = 0;
+            int continuity = 1;
+
+            for (int index = 0; index < testcase.length; index++) {
+                if (testcase[index].equals("O")) {
+                    answer += continuity;
+                    continuity += 1;
+                } else {
+                    continuity = 1;
+                }
+            }
+            System.out.println(answer);
+        }
+    }
+}
