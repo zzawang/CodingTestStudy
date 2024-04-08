28a431624c64b7b4f5fecc37ab4b13800c1dd473
최호빈 <hobinchoi0116@gmail.com>
2024-04-08 23:43:50 +0900

    [Gold V] Title: 상어 초등학교, Time: 224 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -1,115 +1,87 @@
 from collections import deque
 import sys
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-def bfs(seats, likes, n, start):
-    q = deque([])
-    for s in start:
-        q.append(s)
 
-    arr = [[-1] * n for _ in range(n)]
-    max = 0
-    answer1 = []  # 1번 조건 만족
-    answer2 = []  # 2번 조건 만족
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
-    # 1번 조건
+def find_pos(likes, classroom):
+    start = []
+    for i1 in range(N):
+        for i2 in range(N):
+            if classroom[i1][i2] == 0: # 자리가 비어있으면
+                start.append((i1, i2))
+
+    q = deque(start)
+    visited = []
+
     while q:
         x, y = q.popleft()
         count = 0
+        empty = 0
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and seats[nx][ny] in likes:
+            if 0 <= nx < N and 0 <= ny < N:
+                if classroom[nx][ny] in likes:
+                    count += 1
+                if classroom[nx][ny] == 0:
+                    empty += 1
+
+        visited.append((x, y, count, empty))
+
+
+    # y가 작은 순서대로
+    visited.sort(key=lambda x: x[1])
+
+    # x가 작은 순서대로
+    visited.sort(key=lambda x: x[0])
+
+    # empty가 큰 순서대로
+    visited.sort(key=lambda x: x[3], reverse=True)
+
+    # count가 큰 순서대로
+    visited.sort(key=lambda x:x[2], reverse=True)
+
+    return (visited[0][0], visited[0][1])
+
+
+def calculate_satisfaction(x, y, classroom):
+    student = classroom[i1][i2]
+    likes = student_likes[student]  # 학생이 좋아하는 4명의 학생들
+
+    count = 0
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < N and 0 <= ny < N:
+            if classroom[nx][ny] in likes:
                 count += 1
 
-        arr[x][y] = count
-        if max < count:
-            max = count
+    return count
 
-    for i1 in range(n):
-        for i2 in range(n):
-            if max == arr[i1][i2]:
-                answer1.append((i1, i2))
-            arr[i1][i2] = -1     # 재활용
+N = int(sys.stdin.readline())
 
-    # 1번 조건을 만족하는 위치가 여러 개라면 2번 조건 체크
-    if len(answer1) == 1:
-        return answer1[0]
-    else:
-        max = 0
-        for a in answer1:
-            q.append(a)
+student_likes = {}  # 각 학생이 좋아하는 학생 4명의 번호
+student_satisfaction = {}  # 각 학생의 만족도
+student_sequence = [] # 학생이 삽입된 순서
+satisfaction = [0, 1, 10, 100, 1000]
 
-        while q:
-            x, y = q.popleft()
-            count = 0
-            for i in range(4):
-                nx, ny = x + dx[i], y + dy[i]
-                if 0 <= nx < n and 0 <= ny < n and seats[nx][ny] == '0':
-                    count += 1
+for _ in range(N*N):
+    s = list(map(int, sys.stdin.readline().split()))
+    student_likes[s[0]] = s[1:]
+    student_sequence.append(s[0])
+    student_satisfaction[s[0]] = 0
 
-            arr[x][y] = count
-            if max < count:
-                max = count
+classroom = [[0 for _ in range(N)] for _ in range(N)]
 
-        for i1 in range(n):
-            for i2 in range(n):
-                if max == arr[i1][i2]:
-                    answer2.append((i1, i2))
+for student in student_sequence:
+    likes = student_likes[student] # 학생이 좋아하는 4명의 학생들
+    x, y = find_pos(likes, classroom) # 학생의 자리 찾기
+    classroom[x][y] = student
 
-        # 2번 조건을 만족하는 위치가 여러 개라면 3번 조건 체크
-        if len(answer2) == 1:
-            return answer2[0]
-        else:
-            answer2.sort(key=lambda x:x[0])
-            x1, y1 = answer2[0]
-            x2, y2 = answer2[1]
-            if x1 != x2:
-                return (x1, y1)
-            else:
-                if y1 < y2:
-                    return (x1, y1)
-                else:
-                    (x2, y2)
 
-def main():
-    n = int(sys.stdin.readline())
-    students = [['0'] for _ in range(n * n)]
+for i1 in range(N):
+    for i2 in range(N):
+        student = classroom[i1][i2]
+        student_satisfaction[student] = calculate_satisfaction(i1, i2, classroom)  # 학생의 만족도 구하기
 
-    # 학생 n*n명의 순서와 각 학생이 좋아 하는 학생
-    studentandlike = [[0] for _ in range(n*n + 1)]
-
-    for i in range(n * n):
-        students[i] = list(sys.stdin.readline().split())
-        studentandlike[int(students[i][0])] = students[i][1:]
-
-    # 자리
-    seats = [['0'] * n for _ in range(n)]
-
-    for student in students:
-        # 학생과 학생이 좋아 하는 리스트
-        s, likes = student[0], student[1:]
-
-        # 조건 만족하는 자리
-        start = []
-        for i1 in range(n):
-            for i2 in range(n):
-                if seats[i1][i2] == '0':
-                    start.append((i1, i2))
-
-        x, y = bfs(seats, likes, n, start)
-        seats[x][y] = s
-
-    total = 0
-    score = [0, 1, 10, 100, 1000]
-    for i1 in range(n):
-        for i2 in range(n):
-            count = 0
-            for i in range(4):
-                nx, ny = i1 + dx[i], i2 + dy[i]
-                if 0 <= nx < n and 0 <= ny < n and seats[nx][ny] in studentandlike[int(seats[i1][i2])]:
-                    count += 1
-
-            total += (score[count])
-    print(total)
-
-main()
+satisfaction_sum = sum(satisfaction[satisfac] for satisfac in student_satisfaction.values())
+print(satisfaction_sum)
