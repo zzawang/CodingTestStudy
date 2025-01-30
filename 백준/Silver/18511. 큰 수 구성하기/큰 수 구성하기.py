3b6adba6833b92da9d5929cc0803973398e64243
최호빈 <hobinchoi0116@gmail.com>
2025-01-30 23:24:33 +0900

    [Silver V] Title: 큰 수 구성하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.py
+++ 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.py
@@ -1,22 +1,17 @@
-from collections import deque
 n, k = map(int, input().split())
-k_nums = list(map(int, input().split()))
-
-answer = []
-power = len(str(n)) # 자릿수에 사용되는 숫자
-q = deque([])
-for k_num in k_nums:
-    q.append((10 ** (power - 1) * k_num, power - 1))
-
-while q:
-    num, pow = q.popleft()
-    if pow == 0:
-        answer.append(num)
-    else:
-        for k_num in k_nums:
-            q.append((10 ** (pow - 1) * k_num, pow - 1))
-            result = num + 10 ** (pow - 1) * k_num
-            if result <= n:
-                q.append((result, pow - 1))
-
-print(max(answer))
+a = list(map(int, input().split()))
+result = 0
+ 
+def dfs(x):
+    global result
+    # n보다 작거나 같은 자연수에 대해서만 확인
+    if x > n:
+        return
+    # K의 원소로만 구성된 가장 큰 수를 저장
+    result = max(x, result)
+    for i in a:
+        # K의 원소로만 구성된 모든 수를 탐색
+        dfs(x * 10 + i)
+ 
+dfs(0)
+print(result)
