028c2038579fe7bb5d1ddaac323361e39ebf743c
최호빈 <hobinchoi0116@gmail.com>
2025-02-05 21:43:55 +0900

    [Silver IV] Title: ATM, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,11 +1,9 @@
-import sys
-n = int(sys.stdin.readline())
-arr = list(map(int, sys.stdin.readline().split()))
-arr.sort()
-length = len(arr)
-sum = 0
-for a in arr:
-    sum += (a*length)
-    length -= 1
+n = int(input())
+time = list(map(int, input().split()))
+time.sort()
+answer = 0
 
-print(sum)
+for i in range(n):
+    answer += sum(time[:i + 1])
+
+print(answer)
