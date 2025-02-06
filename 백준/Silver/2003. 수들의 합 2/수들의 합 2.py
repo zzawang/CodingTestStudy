3291561cb7f62300da9eba2581f3d50ebe2cff88
최호빈 <hobinchoi0116@gmail.com>
2025-02-06 13:43:46 +0900

    [Silver IV] Title: 수들의 합 2, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,18 +1,23 @@
-n, m = map(int , input().split())
+n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 
 left = 0
-right = 1
+right = 0
 answer = 0
+current_sum = 0  # 누적 합 저장
 
-while n >= right >= left:
-    result = sum(nums[left:right])
-    if result == m:
-        answer += 1
-        right += 1
-    elif result < m:
-        right += 1
-    elif result > m:
+while right < n:
+    # right를 확장하면서 값 추가
+    current_sum += nums[right]
+    right += 1
+
+    # 현재 합이 m을 초과하면 left를 이동하며 줄이기
+    while current_sum > m and left < right:
+        current_sum -= nums[left]
         left += 1
+
+    # 원하는 합을 찾으면 정답 카운트 증가
+    if current_sum == m:
+        answer += 1
 
 print(answer)
