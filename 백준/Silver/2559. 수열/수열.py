a66aacd9328c985774447f3d5f325e01243d6e63
최호빈 <hobinchoi0116@gmail.com>
2025-02-06 14:26:03 +0900

    [Silver III] Title: 수열, Time: 72 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,16 +1,11 @@
 n, k = map(int, input().split())
 temps = list(map(int, input().split()))
 
-left = 0
-right = k - 1
-result = sum(temps[left:right + 1])
-answer = result
+result = sum(temps[:k])
+answer = [result]
 
-while right < n - 1:
-    right += 1
-    result += (temps[right] - temps[left])
-    left += 1
-    answer = max(answer, result)
+for i in range(n - k):
+    result += (temps[i + k] - temps[i])
+    answer.append(result)
 
-
-print(answer)
+print(max(answer))
