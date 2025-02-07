d8ab9a6e07cfd4cba0d3e125c4e81dfa062f065f
최호빈 <hobinchoi0116@gmail.com>
2025-02-08 00:43:06 +0900

    [Silver II] Title: N과 M (10), Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,32 @@
+import sys
+
+n, m = 0, 0
+nums = []  # 수열
+arr = [] # N개의 자연수 중에서 M개를 고른 수열
+visited = [] # 수 방문 여부
+
+def dfs(idx, length):
+    global n, m, nums, arr, visited
+
+    if length == m:
+        print(*arr)
+        return
+
+    prev = 0
+    for i in range(idx, n):
+        if not visited[i] and prev != nums[i]:
+            visited[i] = True
+            arr.append(nums[i])
+            prev = nums[i]
+            dfs(i, length + 1)
+            arr.pop()
+            visited[i] = False
+
+def solution():
+    global n, m, nums, visited
+    n, m = map(int, sys.stdin.readline().split())
+    nums = sorted(list(map(int, sys.stdin.readline().split())))
+    visited = [False] * n
+    dfs(0, 0)
+
+solution()
