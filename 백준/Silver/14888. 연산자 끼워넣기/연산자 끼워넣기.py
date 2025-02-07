c21f9073c0cd8b854471721f9fda59b76be46b19
최호빈 <hobinchoi0116@gmail.com>
2025-02-08 01:35:15 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -3,52 +3,44 @@ import sys
 n = 0
 nums = []
 sign = []
-expression = []
 answer = []
-visited = []
 
-def dfs(length):
-    global n, nums, s, answer, visited, sign, expression
+def dfs(length, temp):
+    global n, nums, sign, answer
 
     if length == n - 1:
-        tmp = nums[0]
-        for s in range(n - 1):
-            if expression[s] == 0:
-                tmp += nums[s + 1]
-            elif expression[s] == 1:
-                tmp -= nums[s + 1]
-            elif expression[s] == 2:
-                tmp *= nums[s + 1]
-            else:
-                if tmp < 0 < nums[s + 1]:
-                    tmp = -(-tmp // nums[s + 1])
-                else:
-                    tmp //= nums[s + 1]
-
-        answer.append(tmp)
+        answer.append(temp)
         return
 
-    for i in range(n - 1):
-        if not visited[i]:
-            visited[i] = True
-            expression.append(sign[i])
-            dfs(length + 1)
-            expression.pop()
-            visited[i] = False
+    # 하부함수 호출
+    if sign[0] > 0:
+        sign[0] -= 1
+        dfs(length + 1, temp + nums[length + 1])
+        sign[0] += 1
 
+    if sign[1] > 0:
+        sign[1] -= 1
+        dfs(length + 1, temp - nums[length + 1])
+        sign[1] += 1
+
+    if sign[2] > 0:
+        sign[2] -= 1
+        dfs(length + 1, temp * nums[length + 1])
+        sign[2] += 1
+
+    if sign[3] > 0:
+        sign[3] -= 1
+        dfs(length + 1, int(temp / nums[length + 1]))
+        sign[3] += 1
 
 def solution():
-    global n, nums, s, answer, visited, sign
+    global n, nums, sign, answer
 
     n = int(sys.stdin.readline())
     nums = list(map(int, sys.stdin.readline().split()))
-    arr = list(map(int, sys.stdin.readline().split()))
-    sign = []
-    for i, v in enumerate(arr):
-        sign.extend([i] * v)
+    sign = list(map(int, sys.stdin.readline().split()))
 
-    visited = [False] * (n - 1)
-    dfs(0)
+    dfs(0, nums[0])
     print(max(answer))
     print(min(answer))
 
