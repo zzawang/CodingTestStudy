303dac87cacf7369328c4ff25830463d89887efc
최호빈 <hobinchoi0116@gmail.com>
2025-02-07 23:47:57 +0900

    [Silver II] Title: 부분수열의 합, Time: 228 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -2,29 +2,26 @@ import sys
 
 n, s = 0, 0
 nums = []  # 수열
-arr = []  # 부분 수열
-count = 0  # 원소를 다 더한 값이 S인 부분 수열의 개수
+answer = 0  # 원소를 다 더한 값이 S인 부분 수열의 개수
 
-def bt(idx):
-    global n, s, nums, arr, count
+def bt(idx, arr_sum, count): # 인덱스, 부분 수열의 합, 부분 수열에 포함된 수의 개수
+    global n, s, nums, answer
 
-    if len(arr) >= n:
-        if sum(arr) == s:
-            count += 1
+    if idx == n:
+        if arr_sum == s and count > 0: # 공집합은 포함 안되므로
+            answer += 1
         return
-    else:
-        if arr and sum(arr) == s:
-            count += 1
-        for i in range(idx, n):
-            arr.append(nums[i])
-            bt(i + 1)
-            arr.pop()
+
+    # 현재 인덱스의 값을 포함하는 경우
+    bt(idx + 1, arr_sum + nums[idx], count + 1)
+    # 현재 인덱스의 값을 포함하지 않는 경우
+    bt(idx + 1, arr_sum, count)
 
 def solution():
-    global n, s, nums, count
+    global n, s, nums, answer
     n, s = map(int, sys.stdin.readline().split())
     nums = sorted(list(map(int, sys.stdin.readline().split())))
-    bt(0)
-    print(count)
+    bt(0, 0, 0)
+    print(answer)
 
 solution()
