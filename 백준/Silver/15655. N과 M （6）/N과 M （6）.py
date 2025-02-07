707135cf3a900fd87b5bae46e07a4daa5e0622af
최호빈 <hobinchoi0116@gmail.com>
2025-02-07 10:54:34 +0900

    [Silver III] Title: N과 M (6), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,31 @@
+n, m = 0, 0
+answer = []
+nums = []
+visited = []
+
+def bt(length):
+    global n, m, answer, nums, visited
+
+    if length == m:
+        print(*answer)
+        return
+
+    for i in range(n):
+        if not visited[i]:
+            if not answer or (answer and answer[-1] < nums[i]):
+                visited[i] = True
+                answer.append(nums[i])
+                bt(length + 1)
+                visited[i] = False
+                answer.pop()
+
+
+def solution():
+    global n, m, nums, visited
+    n, m = map(int, input().split())
+    nums = list(map(int, input().split()))
+    nums.sort()
+    visited = [False] * (n + 1)
+    bt(0)
+
+solution()
