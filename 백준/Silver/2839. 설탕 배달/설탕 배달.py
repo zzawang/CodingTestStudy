fe0dac67359b5dd28aa678b9bd34d4ebfb733bd1
최호빈 <hobinchoi0116@gmail.com>
2025-02-19 02:25:13 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,14 +1,21 @@
-def main():
-    n = int(input())
+n = int(input())
 
-    for i in range(n//5, 0, -1):
-        if (n - 5*i)%3 == 0:
-            print(i+(n - 5*i)//3)
-            return
+five = [-1] * 5001
+five[3], five[5] = 0, 1
 
-    if n%3 == 0:
-        print(n//3)
-    else:
-        print(-1)
+three = [-1] * 5001
+three[3], three[5] = 1, 0
 
-main()
+for i in range(3, 5001):
+    if five[i - 5] != -1:
+        five[i] = five[i - 5] + 1
+        three[i] = 0
+    if i % 5 != 0 and three[i - 3] != -1:
+        three[i] = three[i - 3] + 1
+        if five[i] == -1:
+            five[i] = 0
+
+if five[n] + three[n] == -2:
+    print(-1)
+else:
+    print(five[n] + three[n])
