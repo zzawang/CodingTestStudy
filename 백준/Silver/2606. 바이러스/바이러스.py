2b9e0f28de61c5718bf070b732f5dc057b21d05e
최호빈 <hobinchoi0116@gmail.com>
2025-02-22 10:09:37 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,23 +1,30 @@
-from collections import deque
+# from collections import deque
 
-def bfs(start):
-    visited = [False] * (n + 1)
+# def bfs(start):
+#     visited = [False] * (n + 1)
+#     visited[start] = True
+#     q = deque([start])
+#     answer = 0
+#
+#     while q:
+#         computer = q.popleft()
+#         for c in computers[computer]: # 네트워크 상에서 직접 연결되어 있는 컴퓨터 순회
+#             if not visited[c]:
+#                 visited[c] = True
+#                 q.append(c)
+#                 answer += 1
+#
+#     return answer
+
+count = 0
+
+def dfs(start, visited):
+    global count
     visited[start] = True
-    q = deque([start])
-    answer = 0
-
-    while q:
-        computer = q.popleft()
-        for c in computers[computer]: # 네트워크 상에서 직접 연결되어 있는 컴퓨터 순회
-            if not visited[c]:
-                visited[c] = True
-                q.append(c)
-                answer += 1
-
-    return answer
-
-# def dfs(start):
-
+    for c in computers[start]:
+        if not visited[c]:
+            count += 1
+            dfs(c, visited)
 
 
 n = int(input())
@@ -29,5 +36,6 @@ for _ in range(pairs):
     computers[p1].append(p2)
     computers[p2].append(p1)
 
-print(bfs(1))
-# print(dfs(1))
+# print(bfs(1))
+dfs(1, [False] * (n + 1))
+print(count)
