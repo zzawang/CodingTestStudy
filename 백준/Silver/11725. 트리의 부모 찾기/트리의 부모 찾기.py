82e254b96d1f33c2e9d57f201bca015055dece0b
최호빈 <hobinchoi0116@gmail.com>
2025-02-22 11:10:37 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 336 ms, Memory: 67000 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,29 +1,26 @@
 import sys
-from collections import deque
-def bfs(node, visited, start, answer):
-    q = deque([start])
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def dfs(start):
     visited[start] = True
-    while q:
-        s = q.popleft()
-        for x in node[s]:
-            if not visited[x]:
-                visited[x] = True
-                answer[x] = s
-                q.append(x)
+
+    for node in nodes[start]:
+        if not visited[node]:
+            parents[node] = start
+            dfs(node)
 
 n = int(input())
-answer = [0 for _ in range(n+1)]
-visited = [False for _ in range(n+1)]
-node = [[] for _ in range(n+1)]
-for _ in range(n-1):
-    a, b = map(int, sys.stdin.readline().split())
-    if b not in node[a]:
-        node[a].append(b)
-        node[b].append(a)
+visited = [False] * (n + 1)
+nodes = [[] for _ in range(n + 1)]
+parents = [0] * (n + 1)
 
-for start in range(1, n+1):
-    if not visited[start]:
-        bfs(node, visited, start, answer)
+for _ in range(n - 1):
+    n1, n2 = map(int, input().split())
+    nodes[n1].append(n2)
+    nodes[n2].append(n1)
 
-for i in range(2, n+1):
-    print(answer[i])
+dfs(1)
+
+for i in range(2, n + 1):
+    print(parents[i])
