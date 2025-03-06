438ebbdbc5a8f86d92c88d21bdc8a1f6ffae5e77
최호빈 <hobinchoi0116@gmail.com>
2025-03-06 19:13:53 +0900

    [Silver V] Title: 수들의 합, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.py
+++ 백준/Silver/1789. 수들의 합/수들의 합.py
@@ -1,9 +1,16 @@
-# 그리디 알고리즘
-n = int(input())
-sum = 1
-count = 1
-while sum <= n:
-    count += 1
-    sum += count
-    
-print(count - 1)
+import sys
+
+def chk(n, S):
+    return n * (n + 1) // 2 > S
+
+S = int(sys.stdin.readline().rstrip())
+left, right = 1, 93000
+
+while left <= right:
+    mid = (left + right) // 2
+    if chk(mid, S):
+        right = mid - 1
+    else:
+        left = mid + 1
+
+print(right)
