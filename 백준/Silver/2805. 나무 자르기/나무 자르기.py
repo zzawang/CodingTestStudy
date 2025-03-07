93fc5672eb5180f74d578a3b9a1e26ab2dfa6155
최호빈 <hobinchoi0116@gmail.com>
2025-03-07 17:54:24 +0900

    [Silver II] Title: 나무 자르기, Time: 2708 ms, Memory: 145580 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,20 +1,22 @@
 import sys
 
+def search():
+    start, end = 0, max(tree)
+    while start <= end:
+        mid = (start + end) // 2
+        result = 0
+        for t in tree:
+            tmp = t - mid
+            if tmp > 0:
+                result += tmp
+
+        if result >= M:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return end
+
 N, M = map(int, sys.stdin.readline().rstrip().split())
 tree = sorted(list(map(int, sys.stdin.readline().rstrip().split())))
-start, end = 0, max(tree)
-
-while start <= end:
-    mid = (start + end) // 2
-    result = 0
-    for t in tree:
-        tmp = t - mid
-        if tmp > 0:
-            result += tmp
-
-    if result >= M:
-        start = mid + 1
-    else:
-        end = mid - 1
-
-print(end)
+print(search())
