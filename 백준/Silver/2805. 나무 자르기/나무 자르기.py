ac549342059d33c6697a42887a949960ea4a6b1f
최호빈 <hobinchoi0116@gmail.com>
2025-03-07 17:48:29 +0900

    [Silver II] Title: 나무 자르기, Time: 2800 ms, Memory: 143532 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,27 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+def binary_search():
+    global ans
+    start, end = 0, max(arr)
+
+    while start <= end:
+        mid = (start + end) // 2
+        total_length = 0
+        for i in arr:
+            if i - mid >= 0:
+                total_length += i - mid
+
+        if total_length < M:
+            end = mid - 1
+        else:
+            start = mid + 1
+    ans = end
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+ans = 0
+binary_search()
+print(ans)
