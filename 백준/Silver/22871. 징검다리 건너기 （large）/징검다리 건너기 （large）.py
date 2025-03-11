0e7d877a6a38d15708ed4e7d371110bc7498017d
최호빈 <hobinchoi0116@gmail.com>
2025-03-11 16:08:59 +0900

    [Silver I] Title: 징검다리 건너기 (large), Time: 2804 ms, Memory: 49700 KB -BaekjoonHub

--- 백준/Silver/22871. 징검다리 건너기 （large）/징검다리 건너기 （large）.py
+++ 백준/Silver/22871. 징검다리 건너기 （large）/징검다리 건너기 （large）.py
@@ -1,25 +1,31 @@
 import sys
+
+# sys.setrecursionlimit(1000000)
+input = sys.stdin.readline
+
 import heapq
+def solution(n, arr):
+    dp = [float('inf')] * n
+    dp[0] = 0
+    min_heap = [(0,0)]
 
-N = int(sys.stdin.readline().rstrip())  # 돌의 개수
-A = list(map(int, sys.stdin.readline().rstrip().split()))
+    while min_heap:
+        cost, i = heapq.heappop(min_heap)
 
-dp = [float('inf')] * N
-dp[0] = 0
-min_heap = [(0, 0)]
+        if cost > dp[i]:
+            continue
 
-while min_heap:
-    cost, i = heapq.heappop(min_heap)
+        for j in range(i+1, n):
+            cost2 = (j-i) * (1 + abs(arr[i] - arr[j]))
+            ncost = max(dp[i], cost2)
 
-    if cost > dp[i]:
-        continue
+            if ncost < dp[j]:
+                dp[j] = ncost
+                heapq.heappush(min_heap, (ncost, j))
 
-    for j in range(i + 1, N):
-        power = (j - i) * (1 + abs(A[i] - A[j]))
-        ncost = max(dp[i], power)
+    return dp[n-1]
 
-        if ncost < dp[j]:
-            dp[j] = ncost
-            heapq.heappush(min_heap, (ncost, j))
 
-print(dp[N - 1])
+n = int(input())
+arr = list(map(int, input().strip().split()))
+print(solution(n, arr))
