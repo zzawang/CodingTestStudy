2a087174cd479ffae8925c65171a26dfa988a03e
최호빈 <hobinchoi0116@gmail.com>
2025-03-11 15:45:55 +0900

    [Silver I] Title: 징검다리 건너기 (large), Time: 6688 ms, Memory: 49700 KB -BaekjoonHub

--- 백준/Silver/22871. 징검다리 건너기 （large）/징검다리 건너기 （large）.py
+++ 백준/Silver/22871. 징검다리 건너기 （large）/징검다리 건너기 （large）.py
@@ -1,14 +1,25 @@
 import sys
+import heapq
 
 N = int(sys.stdin.readline().rstrip())  # 돌의 개수
 A = list(map(int, sys.stdin.readline().rstrip().split()))
 
-dp = [float("inf")] * N
+dp = [float('inf')] * N
 dp[0] = 0
+min_heap = [(0, 0)]
 
-for j in range(N):
-    for i in range(j):
-        power = max(dp[i], (j - i) * (1 + abs(A[i] - A[j])))
-        dp[j] = min(dp[j], power)
+while min_heap:
+    cost, i = heapq.heappop(min_heap)
 
-print(dp[-1])
+    if cost > dp[i]:
+        continue
+
+    for j in range(i + 1, N):
+        power = (j - i) * (1 + abs(A[i] - A[j]))
+        ncost = max(dp[i], power)
+
+        if ncost < dp[j]:
+            dp[j] = ncost
+            heapq.heappush(min_heap, (ncost, j))
+
+print(dp[N - 1])
