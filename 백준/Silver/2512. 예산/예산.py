9d7ac3ef5fc20ff7c8d1c9a59d6c37f27bc36388
최호빈 <hobinchoi0116@gmail.com>
2025-03-11 14:42:50 +0900

    [Silver II] Title: 예산, Time: 60 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -2,27 +2,22 @@ import sys
 
 N = int(sys.stdin.readline().rstrip())  # 지방의 수
 budgets = sorted(list(map(int, sys.stdin.readline().rstrip().split())))
-M = int(sys.stdin.readline().rstrip()) # 총 예산
-max_budget = max(budgets)
-sum_budget = sum(budgets)
+M = int(sys.stdin.readline().rstrip())  # 총 예산
+answer = 0
+start, end = 1, max(budgets)
 
-if M >= sum_budget:
-    print(max_budget)
-else:
-    answer = 0
-    start, end = 1, max_budget
-    while start <= end:
-        mid = (start + end) // 2
-        tmp = 0
-        for b in budgets:
-            if mid - b >= 0:
-                tmp += b
-            else:
-                tmp += mid
-
-        if tmp > M:  # 정해진 예산을 초과하면
-            end = mid - 1
+while start <= end:
+    mid = (start + end) // 2
+    tmp = 0
+    for b in budgets:
+        if mid - b >= 0:
+            tmp += b
         else:
-            start = mid + 1
+            tmp += mid
 
-    print(end)
+    if tmp > M:  # 정해진 예산을 초과하면
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(end)
