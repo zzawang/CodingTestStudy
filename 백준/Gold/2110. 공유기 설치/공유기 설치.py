1fbde92e18c9ef06f0f344e6e2164f69fc5e512e
최호빈 <hobinchoi0116@gmail.com>
2025-03-24 17:31:25 +0900

    [Gold IV] Title: 공유기 설치, Time: 672 ms, Memory: 40140 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -7,25 +7,21 @@ for _ in range(N):
 arr.sort()
 
 start, end = 1, arr[-1] - arr[0]  # 최소 공유기 거리, 최대 공유기 거리
-answer = 0
-
 # 두 공유기 사이의 거리 찾기
 while start <= end:
     mid = (start + end) // 2  # 현재 공유기 거리
     current = arr[0]
     count = 1
 
-    # 공유기 설치 몇 대 할 수 있는지 체크
+    # 공유기 설치 개수 구하기
     for i in range(1, len(arr)):
         if arr[i] >= current + mid:
             count += 1
             current = arr[i]
-    # 공유기 설치 수가 목표 보다 크면 공유기 사이 거리 늘림
-    if count >= C:
+    
+    if count >= C:  # 공유기 설치 수가 목표 보다 크면 공유기 사이 거리 늘리기
         start = mid + 1
-        answer = mid
-    # 공유기 설치 수가 목표 보다 작으면 공유기 사이 거리 줄임
-    else:
+    else:  # 공유기 설치 수가 목표 보다 작으면 공유기 사이 거리 줄이기
         end = mid - 1
 
 print(end)
