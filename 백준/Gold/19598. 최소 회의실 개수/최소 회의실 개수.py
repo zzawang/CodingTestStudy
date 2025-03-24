a8538897bc4313c2ff0fce852aae7fe99de4633f
최호빈 <hobinchoi0116@gmail.com>
2025-03-24 22:31:34 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 332 ms, Memory: 56292 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
@@ -2,24 +2,21 @@ import sys
 import heapq
 
 N = int(sys.stdin.readline().rstrip())
-events = []
+times = []
 
 for _ in range(N):
-    s, e = map(int, sys.stdin.readline().split())
-    heapq.heappush(events, (s, 's'))  # 시작 이벤트
-    heapq.heappush(events, (e, 'e'))  # 종료 이벤트
+    times.append(list(map(int, sys.stdin.readline().rstrip().split())))
 
-answer = 0
-rooms = 0
+times.sort(key=lambda x:x[0])  # 시작시간을 기준으로 정렬
 
-while events:
-    time, event_type = heapq.heappop(events)
+arr = [0]  # 최소 힙을 사용하기 위해 0으로 초기화
+count = 1
 
-    if event_type == 's':  # 회의 시작
-        rooms += 1
-    else:  # 회의 종료
-        rooms -= 1
-
-    answer = max(answer, rooms)  # 최대 회의실 개수 갱신
-
-print(answer)
+for start, end in times:
+    if start >= arr[0]: # 진행 중인 강의 종료시간보다 시작시간이 늦거나 같다면 강의 가능
+        heapq.heappop(arr)
+    else:
+        count += 1
+        
+    heapq.heappush(arr, end)
+print(count)
