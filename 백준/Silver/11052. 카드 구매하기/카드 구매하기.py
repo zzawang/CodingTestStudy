35819dd8c72588389cb64f55cd3418b9ee00d08a
최호빈 <hobinchoi0116@gmail.com>
2025-04-18 15:19:30 +0900

    [Silver I] Title: 카드 구매하기, Time: 172 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,11 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+cards = [0] + list(map(int, sys.stdin.readline().rstrip().split()))
+dp = [0] * (N + 1)
+
+for i in range(1, N + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i - j] + cards[j])
+
+print(dp[N])
